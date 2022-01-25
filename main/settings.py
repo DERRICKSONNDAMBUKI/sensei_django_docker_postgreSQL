07dc98ebe1508ba9c2e3466bcd6a51aefdb87709
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-25 13:35:57 +0300

    psycopg2-binary error commit

--- main/settings.py
+++ main/settings.py
@@ -75,8 +75,19 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # sqlite3
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        # postgreSQL
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'postgres',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : 'db',
+        'PORT' : 5432
+
     }
 }
 
